2252cc50dbea339815dc56dde5bb61e51781fbb9
Devan Kavalchek <devankav7@gmail.com>
2024-11-18 16:33:17 -0500

    Update persist file types

--- after/plugin/persisted.lua
+++ after/plugin/persisted.lua
@@ -11,7 +11,9 @@ vim.api.nvim_create_autocmd("User", {
     pattern = "PersistedSavePre",
     callback = function()
         for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-            if vim.bo[buf].filetype == "SidebarNvim" then
+            local filetype = vim.bo[buf].filetype
+
+            if filetype == "SidebarNvim" or filetype == "cmd_menu"  then
                 vim.api.nvim_buf_delete(buf, { force = true })
             end
         end

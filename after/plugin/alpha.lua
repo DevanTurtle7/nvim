60810265cdf4bf44cc75c982eef605dea7f3a168
DevanTurtle7 <devankav7@gmail.com>
2025-01-10 03:11:02 -0500

    Update alpha screen

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -2,17 +2,29 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 local ascii = require("ascii")
 
+
+local explorer_button = dashboard.button("SPC f", "  Jump to explorer")
+local file_button = dashboard.button("SPC p f", "󰈞  Find file")
+local word_button = dashboard.button("SPC p s", "󰈬  Find word")
+local harpoon_button = dashboard.button("CTRL e", "  Open harpoon", "<C-e>")
+
+explorer_button.opts.hl_shortcut = "Comment"
+file_button.opts.hl_shortcut = "Comment"
+word_button.opts.hl_shortcut = "Comment"
+harpoon_button.opts.hl_shortcut = "Comment"
+
 --[[
 Icons:
  󰈞 󰊄  󰈬  
 ]]--
 
+dashboard.section.header.opts.hl = "Comment"
 dashboard.section.header.val = ascii.art.misc.hydra.hydra
 dashboard.section.buttons.val = {
-    dashboard.button("SPC f", "  Jump to explorer"),
-    dashboard.button("SPC p f", "󰈞  Find file"),
-    dashboard.button("SPC p s", "󰈬  Find word"),
-    dashboard.button("CTRL e", "  Open harpoon", "<C-e>"),
+    explorer_button,
+    file_button,
+    word_button,
+    harpoon_button,
 }
 
 alpha.setup(dashboard.opts)
